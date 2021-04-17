413f655324300bdf9e7529d6a714e71ec1e50eb7
Mariana <mariana@fastmail.net>
2021-04-17 10:26:09 +0200

    add a realm method

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -13,6 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        do {
+            _ = try Realm()
+        } catch {
+            print("Error initialising new realm: \(error)")
+        }
+        
         return true
     }
 
